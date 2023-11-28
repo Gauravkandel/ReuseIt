8851e2a875f78c7842cdd82995e60fd8cf832355
Gauravkandel <gauravkandell990@gmail.com>
2023-11-28 18:27:00 +0545

    recommend_updated

--- app/Http/Controllers/RecommendationController.php
+++ app/Http/Controllers/RecommendationController.php
@@ -46,27 +46,24 @@ class RecommendationController extends Controller
         $user_id = $request->input('user_id');
 
         if (!$user_id) {
-            return response()->json(['recommendations' => null]);
+            return response()->json(['recommendations' => null], 400);
         }
-        $count = Recommendation::where('user_id', $user_id)->count();
-        if ($count >= 5) {
-            $categoryRecommendations = Recommendation::where('user_id', $user_id)
-                ->orderByDesc('count')
-                ->take(5)
-                ->pluck('category_name');
-            $products = Product::with('image')->where('user_id', '!=', $user_id)
-                ->whereIn('category_id', function ($query) use ($categoryRecommendations) {
-                    $query->select('id')
-                        ->from('categories')
-                        ->whereIn('category_name', $categoryRecommendations);
-                })
-                ->inRandomOrder()
-                ->limit(10)
-                ->get();
 
-            return response()->json(['recommendations' => $products], 200);
-        } else {
-            return response()->json(['recommendations' => null]);
-        }
+        $categoryRecommendations = Recommendation::where('user_id', $user_id)
+            ->orderByDesc('count')
+            ->take(5)
+            ->pluck('category_name');
+
+        $products = Product::where('user_id', '!=', $user_id)
+            ->whereIn('category_id', function ($query) use ($categoryRecommendations) {
+                $query->select('id')
+                    ->from('categories')
+                    ->whereIn('category_name', $categoryRecommendations);
+            })
+            ->inRandomOrder()
+            ->limit(10)
+            ->get();
+
+        return response()->json(['recommendations' => $products], 200);
     }
 }
